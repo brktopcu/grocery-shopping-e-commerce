18aad39b9eda60bb0c08ba63600f2953753ec239
rikianrux <buraktopcu12@hotmail.com>
2018-07-26 17:30:30 +0300

    Şimdilik session kontrolü yapmıyor, onu ekleyebilmek için oturum açtıktan sonra aktivasyon sayfasına ulaşabilmem gerekiyor

--- zeytin/zeytin/aktive.aspx
+++ zeytin/zeytin/aktive.aspx
@@ -1,3 +1,25 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master1.Master" AutoEventWireup="true" CodeBehind="aktive.aspx.cs" Inherits="zeytin.aktive" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <form id="aktivasyonForm" runat="server">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+
+                    <h3>Lütfen aktivasyon kodunu girin:</h3>
+                </div>
+                <div class="col-md-3">
+                    <asp:TextBox ID="aktivasyon" runat="server"></asp:TextBox>
+                </div>
+                  <div class="col-md-3">
+                      <asp:Button ID="aktivasyonGonder" runat="server" Text="Gönder" CssClass="btn" OnClick="aktivasyonGonder_Click" />
+                </div>
+            </div>
+            <br />
+            <asp:Label ID="lblAktivasyon" runat="server" Visible="false"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
 </asp:Content>
--- zeytin/zeytin/aktive.aspx.cs
+++ zeytin/zeytin/aktive.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +10,57 @@ using System.Web.UI.WebControls;
 
 namespace zeytin
 {
+    
     public partial class aktive : System.Web.UI.Page
     {
+
+       
         protected void Page_Load(object sender, EventArgs e)
         {
+            lblAktivasyon.Visible = false;
+        }
 
+        protected void aktivasyonGonder_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            
+            conn.Open();
+            cmd.CommandText = "Select COUNT(1) from Kullanicilar k where k.aktiveKodu = @aktiveKodu";
+            cmd.Parameters.AddWithValue("@aktiveKodu", aktivasyon.Text);
+            cmd.Connection = conn;
+            int sayi = Convert.ToInt32(cmd.ExecuteScalar());
+
+            if (sayi>=1)
+            {
+                lblAktivasyon.Visible = true;
+                lblAktivasyon.Text = "Aktivasyon işlemi başarılı.";
+                lblAktivasyon.ForeColor = System.Drawing.Color.Green;
+
+                SqlCommand cmd2 = new SqlCommand();
+                cmd2.CommandText = "update Kullanicilar set aktifMi = 1 where aktiveKodu = @aktiveKodu";
+                cmd2.Parameters.AddWithValue("@aktiveKodu", aktivasyon.Text);
+                cmd2.Connection = conn;
+                cmd2.ExecuteNonQuery();
+
+            }
+
+            else
+            {
+                lblAktivasyon.Visible = true;
+                lblAktivasyon.Text = "Aktivasyon işlemi başarısız.";
+                lblAktivasyon.ForeColor = System.Drawing.Color.Red;
+
+            }
+            
+           
+
+
+           
+            conn.Close();
+            
         }
     }
 }
